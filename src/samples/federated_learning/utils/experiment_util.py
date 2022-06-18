638ec933c25bfe55252547d4e4435883ef79eadb
Denise-Phi Khuu <denisekhuu@hotmail.de>
2022-06-18 18:40:25 +0200

    results

--- src/samples/federated_learning/utils/experiment_util.py
+++ src/samples/federated_learning/utils/experiment_util.py
@@ -17,15 +17,26 @@ def run_round(client_plane, server, rounds):
     selected_clients = server.select_clients()
     client_parameters = client_plane.train_selected_clients(selected_clients)
     server.aggregate_model(client_parameters)
-
+    
+def run_round_with(selected_clients, nn_params, client_plane, server, rounds):
+    # Federated Learning Round with preselected nn_params and clients
+    set_rounds(client_plane, server, rounds)
+    client_plane.update_clients(server.get_nn_parameters())
+    client_parameters = client_plane.train_selected_clients(selected_clients)
+    server.aggregate_model(client_parameters)
+    
 def select_random_clean(client_plane, config, n):
+    if len(client_plane.poisoned_clients) == config.NUMBER_OF_CLIENTS: 
+        return []
     clean = [x for x in range(config.NUMBER_OF_CLIENTS) if x not in client_plane.poisoned_clients]
     random.shuffle(clean)
     indices = clean[:n]
     return indices
 
 def select_poisoned(client_plane, n):
+    if len(client_plane.poisoned_clients) > 0:
         return client_plane.poisoned_clients[:n]
+    return []
 
 def train_client(client_plane, rounds, idx): 
     client_plane.clients[idx].train(rounds)
